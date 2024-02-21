6ba35eac03266b6b724e60da5c48dcab42dcecbc
marc <marc.espinosa-serra.external@capgemini.com>
2024-02-21 11:44:23 +0100

    Third commit - test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import classes.Polymorphism;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        Polymorphism polymorphism = new Polymorphism();
+        Polymorphism.Cylinder cylinder = polymorphism.new Cylinder();
+        Polymorphism.Circle circle = polymorphism.new Circle();
+
+        cylinder.setHeight(10);
+        cylinder.setRadius(3);
+        System.out.println("Cylinder surface area" + cylinder.calculateArea());
     }
 }
